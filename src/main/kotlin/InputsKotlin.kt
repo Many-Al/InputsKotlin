fcb7f02ca96ad182e7d9b43fc48294e207c90a27
mahmoudalame <alamemany27@gmail.com>
2023-11-14 23:45:51 +0100

    Initial commit

---
+++ src/main/kotlin/InputsKotlin.kt
@@ -0,0 +1,19 @@
+/*manyDiese App ruft Benutzerinformationen ab und druckt sie dann aus
+Wir haben es im Rahmen einer Lernsitzung gemacht
+*/
+
+
+fun main(){
+    //Nach Benutzernamen fragen
+    print("Enter Name: ")
+    val name    = readLine()!!.toString()
+    //Nach dem Alter fragen
+    print("Enter Age: ")
+    val age  = readLine()!!.toInt()
+    print("Enter GPA: ")
+    val GPA  =  readLine()!!.toDouble()
+    println("===== User info ===== ")
+    println("Your Name is : $name")
+    println("Your Age: $age"   )
+    println("Your GPA: $GPA" )
+}
